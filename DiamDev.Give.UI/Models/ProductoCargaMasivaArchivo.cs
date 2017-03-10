7997924d87431b79ce9be3618aef9f0ee53ac97d
Geovanny Campos <geocamposm@gmail.com>
2017-03-10 00:29:34 -0600

    se implementó la carga masiva de productos

---
+++ DiamDev.Give.UI/Models/ProductoCargaMasivaArchivo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiamDev.Give.UI.Models
+{
+    public class ProductoCargaMasivaArchivo
+    {
+        public string Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Correlativo { get; set; }
+    }
+}
